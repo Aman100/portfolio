457707c025d098ab1c9508a9a1eb87d343779dab
aman warudkar <amanwarudkar1@gmail.com>
2021-03-19 14:40:35 +0530

    First Commit

--- src/components/body/AboutMe.js
+++ src/components/body/AboutMe.js
@@ -14,7 +14,7 @@ return (
 <h1 style={mystyle}>About Me</h1>
 <div className='aboutMe'>
 <p>
-Hi, my name is Aman Warudkar, I am straightforward and passionate about designing things and have keen interest in Full-Stack Development. I am an Introspective person, I think a lot to know the Importance of something before getting into it. But when I decide to get into something, I do everything to get the things done. This attitude drives me to learn new tool and technologies. To be more generic this attitude drives me to learn new skills in life.<br/>
+Hi, my name is Aman Warudkar, I am straightforward and passionate about designing things and have keen interest in Project Development. I am an Introspective person, I think a lot to know the Importance of something before getting into it. But when I decide to get into something, I do everything to get the things done. This attitude drives me to learn new tool and technologies. To be more generic this attitude drives me to learn new skills in life.<br/>
 Apart from technological aspect, I love Travelling which enables me to Explore the world better . I like playing Video-Games, Chess, Reading Books.
 </p>
 </div>
